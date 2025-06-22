fa8581f74757850b5c3dbf756d8847d84e38466f
Tim Kempkens <tim.kempkens@googlemail.com>
2025-06-22 07:51:04 +0200

    stuff

--- OrchesterApp.Api/OrchesterApp.Api/Frontend/src/environments/environment.ts
+++ OrchesterApp.Api/OrchesterApp.Api/Frontend/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-   basePathBackend: 'http://localhost:40181/',
-   basePathFrontend: 'http://localhost:8100/'
+   //basePathBackend: 'http://localhost:40181/',
+   //basePathFrontend: 'http://localhost:8100/',
+    basePathBackend: 'https://orchester-app.tim-kempkens.com/',
+    basePathFrontend: 'https://orchester-app.tim-kempkens.com/'
 };
